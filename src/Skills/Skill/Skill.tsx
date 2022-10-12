fd6f4b032af677f50c9ce15ec39847b3eae25ea7
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-12 23:30:21 +0300

    add skills and skill

---
+++ src/Skills/Skill/Skill.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './skill.module.css';
+
+
+type SkillType = { title: string , description : string };
+const Skill: React.FC<SkillType> = (props) => {
+    const {title,description} = props
+    return (
+        <div className={style.skill}>
+            <div className={style.icon}></div>
+            <h3>{title}</h3>
+            <span className={style.description}>{description}</span>
+        </div>
+    );
+};
+
+export {Skill};
