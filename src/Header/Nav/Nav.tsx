10ebb7b2098ffcc2c3527b4c7540758634ca61ec
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-10-13 21:31:11 +0300

    fix style

--- src/Header/Nav/Nav.tsx
+++ src/Header/Nav/Nav.tsx
@@ -6,10 +6,10 @@ const Nav = () => {
     return (
         <div className={style.nav}>
             <ul className={style.lists}>
-                <li className={style.list}><a href="#">Главная</a></li>
-                <li className={style.list}><a href="#">Скиллы</a></li>
-                <li className={style.list}><a href="#">Проекты</a></li>
-                <li className={style.list}><a href="#">Контакты</a></li>
+                <li className={style.list}><a className={style.link} href="#">Главная</a></li>
+                <li className={style.list}><a className={style.link} href="#">Скиллы</a></li>
+                <li className={style.list}><a className={style.link} href="#">Проекты</a></li>
+                <li className={style.list}><a className={style.link} href="#">Контакты</a></li>
             </ul>
 
 
